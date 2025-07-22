ea9571accd5f894b8bd0960a4b6111f50b6ddf19
leangvakhim <Vakhiml60@gmail.com>
2025-07-22 18:14:51 +0700

    init

--- src/component/MenuIcon.jsx
+++ src/component/MenuIcon.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const MenuIcon = () => {
+    const handleToggleSidebar = () => {
+        const sidebar = document.querySelector('#application-sidebar-brand');
+        if (window.HSOverlay && sidebar) {
+            window.HSOverlay.open(sidebar);
+        }
+    };
+
     return (
-        <li class="relative xl:hidden">
-            <a
-                class="text-xl icon-hover cursor-pointer text-heading"
+        <li className="relative xl:hidden">
+            <button
+                onClick={handleToggleSidebar}
+                className="text-xl icon-hover cursor-pointer text-heading"
                 id="headerCollapse"
-                data-hs-overlay="#application-sidebar-brand"
                 aria-controls="application-sidebar-brand"
                 aria-label="Toggle navigation"
-                >
-                <i class="ti ti-menu-2 relative z-1"></i>
-            </a>
+            >
+                <i className="ti ti-menu-2 relative z-1"></i>
+            </button>
         </li>
-    )
-}
+    );
+};
 
-export default MenuIcon
+export default MenuIcon;
