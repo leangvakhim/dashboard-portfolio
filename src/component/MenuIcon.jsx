aeb7725ef2016a7c797ea54cfff7e14ed2a408e1
leangvakhim <Vakhiml60@gmail.com>
2025-07-22 17:53:35 +0700

    Update MenuIcon.jsx

--- src/component/MenuIcon.jsx
+++ src/component/MenuIcon.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 const MenuIcon = () => {
-    return (
-        <li class="relative xl:hidden">
-            <a class="text-xl  icon-hover cursor-pointer text-heading"
-                id="headerCollapse" data-hs-overlay="#application-sidebar-brand"
-                aria-controls="application-sidebar-brand" aria-label="Toggle navigation" >
-                <i class="ti ti-menu-2 relative z-1"></i>
-            </a>
-        </li>
-    )
-}
+    useEffect(() => {
+        // Ensure HSOverlay is initialized after mount
+        if (typeof window !== 'undefined' && window.HSOverlay?.init) {
+            window.HSOverlay.init();
+        }
+    }, []);
 
-export default MenuIcon
+    const handleToggleSidebar = () => {
+        const sidebar = document.querySelector('#application-sidebar-brand');
+        if (window.HSOverlay && sidebar) {
+            window.HSOverlay.open(sidebar);
+        }
+    };
+
+    return (
+        <li className="relative xl:hidden">
+            <button
+                onClick={handleToggleSidebar}
+                className="text-xl icon-hover cursor-pointer text-heading"
+                aria-label="Toggle navigation"
+            >
+                <i className="ti ti-menu-2 relative z-1"></i>
+            </button>
+        </li>
+    );
+};
+
+export default MenuIcon;
