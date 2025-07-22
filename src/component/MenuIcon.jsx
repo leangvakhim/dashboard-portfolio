6002293b61d4a884a86283b0ad2db0917974573c
leangvakhim <Vakhiml60@gmail.com>
2025-07-22 18:17:34 +0700

    Update MenuIcon.jsx

--- src/component/MenuIcon.jsx
+++ src/component/MenuIcon.jsx
@@ -1,26 +1,19 @@
-import React from 'react';
+import React from 'react'
 
 const MenuIcon = () => {
-    const handleToggleSidebar = () => {
-        const sidebar = document.querySelector('#application-sidebar-brand');
-        if (window.HSOverlay && sidebar) {
-            window.HSOverlay.open(sidebar);
-        }
-    };
-
     return (
-        <li className="relative xl:hidden">
-            <button
-                onClick={handleToggleSidebar}
-                className="text-xl icon-hover cursor-pointer text-heading"
+        <li class="relative xl:hidden">
+            <a
+                class="text-xl icon-hover cursor-pointer text-heading"
                 id="headerCollapse"
+                data-hs-overlay="#application-sidebar-brand"
                 aria-controls="application-sidebar-brand"
                 aria-label="Toggle navigation"
-            >
-                <i className="ti ti-menu-2 relative z-1"></i>
-            </button>
+                >
+                <i class="ti ti-menu-2 relative z-1"></i>
+            </a>
         </li>
-    );
-};
+    )
+}
 
-export default MenuIcon;
+export default MenuIcon
