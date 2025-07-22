bd7d1bbb849691048d170af83093f75fdd16bb7c
leangvakhim <Vakhiml60@gmail.com>
2025-07-22 17:33:00 +0700

    Update MenuIcon.jsx

--- src/component/MenuIcon.jsx
+++ src/component/MenuIcon.jsx
@@ -5,7 +5,7 @@ const MenuIcon = () => {
         <li class="relative xl:hidden">
             <a class="text-xl  icon-hover cursor-pointer text-heading"
                 id="headerCollapse" data-hs-overlay="#application-sidebar-brand"
-                aria-controls="application-sidebar-brand" aria-label="Toggle navigation" href="javascript:void(0)">
+                aria-controls="application-sidebar-brand" aria-label="Toggle navigation" >
                 <i class="ti ti-menu-2 relative z-1"></i>
             </a>
         </li>
